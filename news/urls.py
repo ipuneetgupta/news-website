dd59af97a1b3fca42c49a86dfa08772a7fda7b49
puneet gupta <punitgupta1811@gmail.com>
2020-04-22 03:10:16 +0530

    made new_detailpage

--- news/urls.py
+++ news/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url , include
 from . import views
 
 urlpatterns = [
+    url(r'^news/(?P<word>.*)/$',views.news_detail,name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import News
-
+from main.models import Main
 # Create your views here.
+def news_detail(request,word):
+    site = Main.objects.get(pk=2)
+    news = News.objects.filter(name=word)
+    return render(request,'front/news_detail.html',{'news':news,'site':site})
