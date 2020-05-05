2d0a26278b578ffc4948be78adde8ca2fd30f8c7
puneet gupta <punitgupta1811@gmail.com>
2020-05-05 16:18:56 +0530

    add term

--- mynews/settings.py
+++ mynews/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ml4cqya!seyty+gzt!&!h*ev#u-f5ot1re#5zqkd28a+ivxys)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',"punit0252.pythonanywhere.com"
